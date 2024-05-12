5a3bdf2c92dfb4efca89eeb1054480e68c287857
Andrei ALDEA <aldea.andrei.977@gmail.com>
2024-05-12 17:11:56 +0300

    fish/omp: added

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    
+    # init OMP
+    oh-my-posh init fish --config '/home/aaldea/.config/oh-my-posh/themes/atomicBit.omp.json' | source;
+end
+
+
